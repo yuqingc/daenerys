bf19e70b976dd6a9c70979e29ccb5756a90e4b28
Matt <mr.chenyuqing@live.com>
2017-11-07 21:22:33 +0800

    bug of unimported webpack fixed

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,3 +1,4 @@
+const webpack = require('webpack');
 const merge = require('webpack-merge');
 const UglifyJSPlugin = require('uglifyjs-webpack-plugin');
 const common = require('./webpack.common.js');
