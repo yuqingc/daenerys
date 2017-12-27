07c4ebf5cee33af3607c5b0d5f4e1530ddc2c760
CHEN Yuqing <yuqing.chen@1mu.club>
2017-12-27 11:17:34 +0800

    remove babel

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -5,7 +5,7 @@ export default class App extends React.Component{
     render(){
         return <div>           
             <h1>Test Template</h1>
-            <p>This is my React App!!!</p>
+            <p>This is my React App!!!what</p>
             <Hello/>
         </div>
     }
